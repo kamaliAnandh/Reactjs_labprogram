d5685385ebb24b54b3a9c5c21feab2510afad226
kamaliAnandh <kamalianand97@gmail.com>
2026-02-24 12:50:14 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,36 +1,29 @@
-import { useEffect, useState } from 'react'
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import { useState } from 'react'
+import Navbar from './Navbar'
 import './App.css'
+import Home from './Home'
+import About from './About'
+import Contact from './contact'
 
- function App() {
-   const[greeting , setGreeting] = useState("");
- 
-  useEffect(()=>{
-    const interval = setInterval(()=>{
-      const now = new Date();
-      const hour = String(now.getHours()).padStart(2,"0");
-      const min = String(now.getMinutes()).padStart(2,"0");
-      const sec = String(now.getSeconds()).padStart(2,"0");
-
-      let msg = ""
-      if (hour < 12) msg = "good morning";
-      else if(hour < 16) msg = "good afternoon"
-      else if(hour <19)  msg = "good evening"
-      else msg = "good night"
-
-      setGreeting(`${msg} <br/> ${hour} : ${min} : ${sec}`);
-
-    },1000);
-
-  
+function App() {
+  <h1>Navbar</h1>
 
   return (
     <>
-      <h1>digital clock</h1>
-      <div>
-        <h1 dangerouslySetInnerHTML={{__html :greeting}}></h1>
-      </div>
+    <BrowserRouter>
+    <h1>navbar component</h1>
+    <Navbar/>
+    <Routes>
+     
+       <Route path='/'element={<Home/>}/>
+       <Route path='/about' element={<About/>} />
+       <Route path='/contact'element={<Contact/>}/>
+    </Routes>
+    
+    </BrowserRouter>
     </>
   )
 }
+
 export default App
