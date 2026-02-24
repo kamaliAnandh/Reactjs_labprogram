2f1147582a50ff729df7083681a64d27d7e3d298
kamaliAnandh <kamalianand97@gmail.com>
2026-02-24 12:48:34 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,26 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter,Route,Contact,Routes} from'react-router-dom'
 import './App.css'
+import Home from './Home'
+import About from './About'
+import Navbar from './Navbar'
+import Contact from './Contact'
+
 function App() {
-  const[num1,setNum1]=useState("");
-  const[num2,setNum2]=useState("");
-
-  const a=Number(num1);
-  const b=Number(num2);
-
-
-  
-  return (
+   return (
     <>
-    <h1>Live Calculator</h1>
-    <input type="number" value={num1} onChange={(e)=>setNum1(e.target.value)} />
-    <input type="number" value={num2} onChange={(e)=>setNum2(e.target.value)} />
-    <h2>Results</h2>
-    <p>Addition:{a+b}</p>
-    <p>Subtraction:{a-b}</p>
-    <p>Multiply:{a*b}</p>
-    <p>Division:{a/b}</p>
-    
-       
+      <h1>Navbar</h1>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/'element={<About/>}/>
+        <Route path='/'element={<Contact/>}/>
+        
+
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
